223b8ffac3fd974b36a3c7e580e6d5d1d4714405
Haroldo Carvalho <carvalhoh@gmail.com>
2023-10-17 18:22:41 -0300

    up

---
+++ JavaScript/script.js
@@ -0,0 +1,81 @@
+// 4 + 3;
+
+// if (4 + 3 === 7) {
+//   alert("you are very smart");
+// }
+
+// function sayHello() {
+//   console.log("hello");
+// }
+// sayHello(); // hello
+
+// function sayHello(name) {
+//   console.log("hello " + name);
+// }
+// sayHello("bob"); // hello bob
+
+// var sayBye = function () {
+//   console.log("Bye!!");
+// };
+
+// sayBye(); // Bye!!
+
+// function sing(song) {
+//   console.log(song);
+// }
+// sing("I am a lumberjack, and I rock");
+// sing("I wanna rock");
+
+// function multiply(a, b) {
+//   if (a > 10 || b > 10) {
+//     return "that's too hard";
+//   } else {
+//     return a * b;
+//   }
+//   console.log(a, b);
+//   return a * b;
+// }
+
+// multiply(12, 10); // undefined
+
+// ARRAYS //
+
+// var list = ["tiger", "cat", "bear", "bird"];
+// console.log(list[1]); // cat
+// for (var i = 0; i < list.length; i++) {
+//   console.log(list[i]);
+// }
+
+// var myList = [
+//   ["tiger", "cat", "bear", "bird"],
+//   ["dog", "fish", "goat", "cow"],
+// ];
+
+// console.log(myList[0][2]); // goat
+
+var user = {
+  name: "John",
+  age: 34,
+  hobby: "Soccer",
+  isMarried: false,
+  spells: ["abrakadabra", "shazam", "boo"],
+  shout: function () {
+    console.log("AHHHHHHHH!");
+  },
+};
+
+var list = [
+  {
+    username: "andy",
+    password: "abcdefg",
+  },
+  {
+    username: "jess",
+    password: "123",
+  },
+];
+// console.log(user.name); // John
+// console.log(user.spells[1]); // shazam
+// console.log(user.shout()); // AHHHHHHHH!
+
+// FUNCTIONS //
---
+++ facebook/script.js
@@ -0,0 +1,21 @@
+var database = [
+  {
+    username: "andrei",
+    password: "supersecret",
+  },
+];
+
+var newsFeed = [
+  {
+    username: "Bobby",
+    timeline: "So tired from all that learning!",
+  },
+  {
+    username: "Sally",
+    timeline: "Javascript is sooooo cool!",
+  },
+  {
+    username: "Mitch",
+    timeline: "Javascript is preeetyy cool!",
+  },
+];
