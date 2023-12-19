1e50c9ffc0c9c907ac2cd51dab3dfa054a8dd3da
Haroldo Carvalho <carvalhoh@gmail.com>
2023-12-19 14:35:21 -0300

    up

--- ES8/script.js
+++ ES8/script.js
@@ -36,7 +36,65 @@ movePlayer(100, "left")
 
 // ASYNC
 async function playerStart() {
-  const firstMove = await movePlayer(100, "left"); // pause
-  await movePlayer(400, "left"); // pause
-  await movePlayer(10, "right"); // pause
+  const first = await movePlayer(100, "left"); // pause
+  const second = await movePlayer(400, "left"); // pause
+  const third = await movePlayer(10, "right"); // pause
+  const fourth = await movePlayer(330, "left"); // pause
 }
+
+// Fetch function
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => response.json())
+  .then(console.log);
+
+// ASYNC AWAIT
+async function fetchUsers() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await resp.json();
+  console.log(data);
+}
+
+const urls = [
+  "https://jsonplaceholder.typicode.com/users",
+  "https://jsonplaceholder.typicode.com/posts",
+  "https://jsonplaceholder.typicode.com/albums",
+];
+
+Promise.all(
+  urls.map((url) => {
+    return fetch(url).then((response) => response.json());
+  })
+)
+  .then((results) => {
+    console.log(results[0]);
+    console.log(results[1]);
+    console.log(results[2]);
+  })
+  .catch(() => console.log("error"));
+
+// Chaining Promises
+promise
+  .then((result) => result + "!")
+  .then((result2) => {
+    console.log(result2);
+  })
+
+  .catch(() => console.log("error!"));
+promise.catch((error) => console.log(error));
+
+// Try catch
+
+const getData = async function () {
+  try {
+    const [users, posts, albums] = await Promise.all(
+      urls.map((url) => {
+        return fetch(url).then((response) => response.json());
+      })
+    );
+    console.log(users);
+    console.log(posts);
+    console.log(albums);
+  } catch (err) {
+    console.log("oops", err);
+  }
+};
